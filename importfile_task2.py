8047e3f84f9949f192a0ba1a5190c6fd45bacfb6
Suvartha Kavali <99177284+Suvartha-DataInsighter@users.noreply.github.com>
2022-03-09 15:41:53 +0530

    Add files via upload

---
+++ importfile_task2.py
@@ -0,0 +1,5 @@
+import task2
+print(task2.factorial(4))
+print(task2.add(10,20))
+print(task2.mul(4,8))
+print(__name__)
---
+++ task2.py
@@ -0,0 +1,30 @@
+import math
+from math import sqrt
+from math import factorial
+
+x=10
+y=15
+
+def add(a,b):
+	return a+b;
+#print(add(1,2))
+def mul(a,b):
+	return a*b;
+#print(mul(2,3))
+def Division(a,b):
+	return a%b;
+#print(Division(4,20))
+def factNum(n):
+	return n
+#print(factorial(4))
+if __name__=='__main__':
+	print(x)
+	print(y)
+	print(add(2,5))
+	print(mul(2,4))
+	print(Division(2,10))
+	print(factorial(5))
+	print(__name__)
+
+
+# A special built in variable
